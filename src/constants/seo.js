4eab61b0abd65bcb64b7ebb4cdd6a94808a93861
dtechoracle <iamezekieljeremiah@gmail.com>
2024-10-31 03:42:51 -0700

    changed the resume

--- src/constants/seo.js
+++ src/constants/seo.js
@@ -1,18 +1,17 @@
-const image =
-  "https://res.cloudinary.com/dkoe20rzl/image/upload/v1692130992/cover_xhnhes.png";
-const creator = "VickyJay";
+// const image =
+//   "https://res.cloudinary.com/dkoe20rzl/image/upload/v1692130992/cover_xhnhes.png";
+const creator = "Dtechoracle";
 const author = creator;
 
 export const url = (slug) => {
-  return `https://victorola.me/${slug ?? ""}`;
+  return `https://dtechoracle-again.vercel.com/${slug ?? ""}`;
 };
 export const SEOdata = {
   homePage: {
     author,
-    image,
     creator,
     url: url(),
-    title: "VickyJay - Creative Frontend Engineer",
+    title: "Dtechoracle - Creative Frontend Engineer",
     description:
       "I enjoy creating frontend solutions with solid foundations, scalability, and excellent user experiences.",
   },
